c29a0559961af06a3d899addf29d3cf353c96498
UserMark <Casinorun00@icloud.com>
2024-05-09 11:57:17 -0400

    Added Attributes for tech

--- TicketWizard/Models/Tech.cs
+++ TicketWizard/Models/Tech.cs
@@ -2,5 +2,11 @@
 {
     public class Tech
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+
     }
 }
